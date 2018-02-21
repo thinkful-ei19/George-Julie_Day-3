2686023dbf75abb244154070d0817c82d0bd01d6
George Colon <geocolon@gmail.com>
2018-02-21 13:48:39 -0800

    complete decoder

---
+++ objectDrills6.js
@@ -0,0 +1,33 @@
+let code = 'craft block argon meter bells brown croon droop'.split(' ');
+let decoder = function(letter) {  
+    const cipher = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4 
+    };
+    return !cipher[letter] ? false : cipher[letter];
+}
+
+function decodeWords(code){
+    // let index = decoder(firstLetter);
+    // code.map( (word=> { let firstLetter = word[0];
+    //     let index = decoder(firstLetter);
+    //     return word[index] || " "; 
+    // }).join('')
+    let result = '';
+    for (let i = 0; i < code.length; i++){
+        let firstLetter = code[i].charAt(0); 
+        const index = decoder(firstLetter);
+        // console.log(index);  
+        if (index){
+            result += code[i].charAt(index);
+        } else {
+            result += ' ';
+        }
+   }
+   return result;
+    
+}
+
+console.log(decodeWords(code));
